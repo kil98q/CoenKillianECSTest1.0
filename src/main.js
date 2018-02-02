fca0e8ab02c858566d539a826a02506b3c25b170
Berend Weij <info@berendweij.nl>
2018-02-02 11:37:19 +0100

    adds first renderer

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import MovingEntity from './entities/movingentity';
-import RenderSystem from './RenderSystem';
 
 export default class Main {
 
@@ -8,19 +7,6 @@ export default class Main {
 
         console.log("base engine running");
 
-        let renderSystem = new RenderSystem();
-
-        let car = new MovingEntity(-300, 200, 14, 0, 'ferrari', {x: 0, y: 0, z:-1});
-
-        renderSystem.add(car);
-        
-		let car2 = new MovingEntity(300, 200, 14, 0, 'ferrari', {x: 0, y: 0, z:-1});
-
-        renderSystem.add(car2);
-        
-		let car3 = new MovingEntity(0, 200, 14, 0, 'ferrari', {x: 0, y: 0, z:-1});
-
-        renderSystem.add(car3);
     }
 
 }
