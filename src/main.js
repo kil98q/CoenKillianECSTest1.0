4a07c5e5ea4af6ce73c480147472046d63cfdc73
Berend Weij <info@berendweij.nl>
2018-02-02 14:57:39 +0100

    updates the htmlrenderer

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 import MovingEntity from './entities/movingentity';
+import HtmlRenderer from './htmlrenderer';
 
 export default class Main {
 
-    constructor(){
+    constructor() {
         // todo: netter als we een echte gameloop hebben
 
         console.log("base engine running");
+
+        const htmlRenderer = new HtmlRenderer();
+
+        for ( let i = 0; i < 5; i++ ){
+            const ferrari = new MovingEntity(-500 + i * 75,
+                                             -400 + Math.random() * 800,
+                                             0,
+                                             0,
+                                             'ferrari',
+                                             { x : 0, y : 0, z : -2 });
+            htmlRenderer.add(ferrari);
+        }
 
     }
 
